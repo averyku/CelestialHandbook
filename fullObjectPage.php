f56962daece3a82a2ecf60c8497c1a41e76c2173
akuboth <139282663+akuboth@users.noreply.github.com>
2023-11-06 10:12:45 -0600

    Full Posts
    - changed some commenting on the index page
    - changed index to only display a list of objects with a link to fullObjectPage for each
    - created a fullObjectPage that uses an id passed through GET to retreive and display all information for that object
    - fullObjectPage will redirect to index when the ID is invalid or no object data is found

---
+++ fullObjectPage.php
@@ -0,0 +1,104 @@
+<?php
+
+/*******w******** 
+    
+    Name: Avery Kuboth
+    Description: WEBD-2013 Project - Celestial Handbook
+    Date: 2023 November 6th
+    Updated: 2023 November 6th
+
+****************/
+
+session_start();
+require('connect.php');
+define('OBJECT_TABLE_NAME', 'celestial_objects');
+
+
+// Redirect the user to the index page
+function redirect()
+{ 
+    header("Location: index.php");
+    die();
+}
+
+// Redirect if no ID was included or ID was not a valid int
+if(!$_GET || empty($_GET['id']) || !filter_input(INPUT_GET,'id',FILTER_VALIDATE_INT))
+    redirect();
+
+// Select the specified object
+$query = 'SELECT * FROM ' . OBJECT_TABLE_NAME . ' WHERE object_id LIKE :id';
+$statement = $db->prepare($query);
+$statement->bindValue(':id', $_GET['id']);
+$statement->execute();
+
+// Redirect if no rows or multiple rows were found
+if ($statement->rowCount() < 1 || 1 > $statement->rowCount())
+    redirect();
+
+// Store the object data
+$object = $statement->fetch();
+
+?>
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="main.css">
+    <title>Celestial Handbook</title>
+</head>
+<body>
+    <div id="content">
+
+        <!-- Header -->
+        <h1><a href="index.php">The Celestial Handbook</a></h1>
+
+        <!-- Login / Out Panel -->
+        <div id="login_box">
+            <?php require('loginModule.php'); ?>
+        </div>
+
+        <br><br>
+
+        <!-- Navigation -->
+        <nav>
+            <ul>
+                <li class="selectedTab"><a href="index.php">Home</a></li>
+                <li class="selectedTab"><a href="https://www.asc-csa.gc.ca/eng/">CSA</a></li>
+                <?php if($_SESSION['login_status'] === 'loggedin' && $_SESSION['login_account']['user_is_admin']): ?>
+                    <li><a href="manageUsers.php">Manage Users</a></li>
+                <?php endif ?>
+            </ul>
+        </nav>
+
+        <br><br><br>
+
+        <!-- All Information About An Object -->
+        <main>
+            <section>
+                <h2>Name: <?= $object['object_name'] ?></h2>
+                <p>Name (scientific): <?= $object['object_scientific_name'] ?></p>
+                <p>ID: <?= $object['object_id'] ?></p>
+                <p>Mass (kg): <?= $object['object_mass_kg'] ?></p>
+                <p>Radius (km): <?= $object['object_radius_km'] ?></p>
+                <p>Distance From Earth (km): <?= $object['object_distance_from_earth'] ?></p>
+                <p>Distance From Sun (km): <?= $object['object_distance_from_sun'] ?></p>
+                <p>Velocity (km/s): <?= $object['object_velocity_kms'] ?></p>
+                <p>Surface Temperature (k): <?= $object['object_surface_temperature_k'] ?></p>
+                <p>Atmosphere: <?= $object['object_has_atmosphere'] ?></p>
+                <p>link: <?= $object['object_media'] ?></p>
+                <img src='<?= $object['object_media'] ?>' width="500" height="300" style="object-fit: contain;" />
+                <br><br><br><br><br><br>
+            </section>
+        </main>
+    
+        <footer><p>Copywrong 2023 - No Rights Reserved</p></footer>
+    </div>
+</body>
+</html>
--- index.php
+++ index.php
@@ -15,15 +15,13 @@ define('OBJECT_TABLE_NAME', 'celestial_objects');
 // define('DATE_FORMAT', 'F d, Y, h:ia');
 
 
-// Select all posts from newest to oldest
+// Select all objects sorted by name
 $query = 'SELECT * FROM ' . OBJECT_TABLE_NAME . ' ORDER BY object_name DESC';
 $statement = $db->prepare($query);
 $statement->execute();
 
 
 ?>
-
-<!-- Adding a comment for testing -->
 
 
 <!DOCTYPE html>
@@ -36,16 +34,19 @@ $statement->execute();
     <title>Celestial Handbook</title>
 </head>
 <body>
-    <!-- Remember that alternative syntax is good and html inside php is bad -->
     <div id="content">
+
+        <!-- Header -->
         <h1><a href="index.php">The Celestial Handbook</a></h1>
 
+        <!-- Login / Out Panel -->
         <div id="login_box">
             <?php require('loginModule.php'); ?>
         </div>
 
         <br><br>
 
+        <!-- Navigation -->
         <nav>
             <ul>
                 <li class="selectedTab"><a href="index.php">Home</a></li>
@@ -57,24 +58,16 @@ $statement->execute();
         </nav>
 
         <br><br><br>
+
+        <!-- List of All Objects -->
         <main>
-            <?php while ($row = $statement->fetch()): ?>
-            <section>
-                <h2>Name: <?= $row['object_name'] ?></h2>
-                <p>Name (scientific): <?= $row['object_scientific_name'] ?></p>
-                <p>ID: <?= $row['object_id'] ?></p>
-                <p>Mass (kg): <?= $row['object_mass_kg'] ?></p>
-                <p>Radius (km): <?= $row['object_radius_km'] ?></p>
-                <p>Distance From Earth (km): <?= $row['object_distance_from_earth'] ?></p>
-                <p>Distance From Sun (km): <?= $row['object_distance_from_sun'] ?></p>
-                <p>Velocity (km/s): <?= $row['object_velocity_kms'] ?></p>
-                <p>Surface Temperature (k): <?= $row['object_surface_temperature_k'] ?></p>
-                <p>Atmosphere: <?= $row['object_has_atmosphere'] ?></p>
-                <p>link: <?= $row['object_media'] ?></p>
-                <img src='<?= $row['object_media'] ?>' width="500" height="300" style="object-fit: contain;" />
-                <br><br><br><br><br><br>
-            </section>
-            <?php endwhile ?>
+            <ul id="object_list">
+                <?php while ($row = $statement->fetch()): ?>
+                    <li>
+                        <a href='fullObjectPage.php?id=<?= $row['object_id'] ?>'><?= $row['object_name'] ?></a>
+                    </li>
+                <?php endwhile ?>
+            </ul>
         </main>
         
         <footer><p>Copywrong 2023 - No Rights Reserved</p></footer>
