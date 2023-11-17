85d9cf60586a9bf7c43c1f50ede392f5936b1dab
akuboth <139282663+akuboth@users.noreply.github.com>
2023-11-17 13:52:35 -0600

    Add/Remove Categories
    - fullObjectPage now requests category_id when querying for a list of the object's categories (needed to remove categories)
    - Added a button on fullObjectPage next to each category to remove the category from the object
    - Added a button on fullObjectPage at the end of the category list to add a new category to the object
    - fullObjectPage has a new query to return a list of all possible categories since admins will have the ability to add those to the object
    - manageCategories page created to process the adding and removing of categories to an object
    - Double values returned from the database are now formatted to be more human readable before they are displayed
    - Fixed a few typos with categories being spelled "catagory" or "catagories"
    - Modified CSS for the category class to ensure new add/remove forms are displayed properly
    - fullObjectPage will now display an object's description

--- fullObjectPage.php
+++ fullObjectPage.php
@@ -5,7 +5,7 @@
     Name: Avery Kuboth
     Description: WEBD-2013 Project - Celestial Handbook
     Date: 2023 November 6th
-    Updated: 2023 November 11th
+    Updated: 2023 November 17th
 
 ****************/
 
@@ -35,9 +35,16 @@ $statement = $db->prepare($query);
 $statement->bindValue(':id', $_GET['id']);
 $statement->execute();
 
-// Select all of the categories
+// Redirect if no rows or multiple rows of objects were found
+if ($statement->rowCount() < 1 || 1 > $statement->rowCount())
+    redirect();
+
+// Store the object data
+$object = $statement->fetch();
+
+// Select all of the categories assigned to the object
 $category_query = '
-SELECT cat.category_name 
+SELECT cat.category_name, cat.category_id
 FROM ' . CAT_TO_OBJ_TABLE . ' cto
 JOIN ' . CATEGORY_TABLE . ' cat ON cto.category_id = cat.category_id
 WHERE cto.object_id LIKE :id';
@@ -45,12 +52,26 @@ $category_statement = $db->prepare($category_query);
 $category_statement->bindValue(':id', $_GET['id']);
 $category_statement->execute();
 
-// Redirect if no rows or multiple rows of objects were found
-if ($statement->rowCount() < 1 || 1 > $statement->rowCount())
-    redirect();
+// Select all of the categories if the user is an admin
+if($_SESSION['login_status'] === 'loggedin' && $_SESSION['login_account']['user_is_admin'])
+{
+    $all_category_query = 'SELECT * FROM ' . CATEGORY_TABLE;
+    $all_category_statement = $db->prepare($all_category_query);
+    $all_category_statement->execute();
+}
 
-// Store the object data
-$object = $statement->fetch();
+// Formats a double/float to be more human readable
+function formatDouble($value)
+{
+    if (strstr(strval($value),"E+"))
+        return str_replace("E+"," x 10^",strval($value));
+    if (strstr(strval($value),"E-"))
+        return str_replace("E-"," x 10^-",strval($value));
+    if ($value >= 1000)
+        return number_format($value,0,".",",");
+
+    return $value;
+}
 
 ?>
 
@@ -99,11 +120,36 @@ $object = $statement->fetch();
         <!-- Testing Only -->
         <p>ID: <?= $object['object_id'] ?></p>
         
-        <!-- Catagories -->
-        <div id="object_catagories">
-        <?php while ($category = $category_statement->fetch()): ?>
-            <div class="category"><?= $category['category_name'] ?></div>
-        <?php endwhile ?>
+        <!-- Categories -->
+        <div id="object_categories">
+            <?php while ($category = $category_statement->fetch()): ?>
+                <div class="category">
+                    <?= $category['category_name'] ?>
+                    <?php if($_SESSION['login_status'] === 'loggedin' && $_SESSION['login_account']['user_is_admin']): ?>
+                        <!-- Remove The Category -->
+                        <form method='post' action='manageCategories.php'>
+                            <input type="hidden" name="object_id" value="<?= $object['object_id'] ?>" ?>
+                            <input type="hidden" name="category_id" value="<?= $category['category_id'] ?>" ?>
+                            <input id="remove_category" name='remove_category' type="submit" value="X">
+                        </form>
+                    <?php endif ?>
+                </div>
+            <?php endwhile ?>
+            <?php if($_SESSION['login_status'] === 'loggedin' && $_SESSION['login_account']['user_is_admin']): ?>
+                <!-- Add New Category -->
+                <div class="category">
+                    <form method='post' action='manageCategories.php'>
+                        <input type="hidden" name="object_id" value="<?= $object['object_id'] ?>" ?>
+                        <input id="add_category" name='add_category' type="submit" value="Add">
+                        <select id="add_category_options" name="category_id">
+                            <option value="invalid">-- Select a Category --</option>
+                            <?php while ($category = $all_category_statement->fetch()): ?>
+                                <option value="<?= $category['category_id'] ?>"><?= $category['category_name'] ?></option>
+                            <?php endwhile ?>
+                        </select>
+                    </form>
+                </div>
+            <?php endif ?>
         </div>
 
         <br><br>
@@ -111,11 +157,11 @@ $object = $statement->fetch();
         <table>
             <tr>
                 <th>Mass:</th>
-                <td><?= $object['object_mass_kg'] ?> kg</td>
+                <td><?= formatDouble($object['object_mass_kg']) ?> kg</td>
             </tr>
             <tr>
                 <th>Radius:</th>
-                <td><?= $object['object_radius'] ?> <?= $object['object_radius_unit'] ?></td>
+                <td><?= formatDouble($object['object_radius']) ?> <?= $object['object_radius_unit'] ?></td>
             </tr>
             <tr>
                 <th>Location:</th>
@@ -123,11 +169,18 @@ $object = $statement->fetch();
             </tr>
             <tr>
                 <th>Distance from <?= $object['object_distance_from'] ?>:</th>
-                <td><?= $object['object_distance'] ?> <?= $object['object_distance_unit'] ?></td>
+                <td><?= formatDouble($object['object_distance']) ?> <?= $object['object_distance_unit'] ?></td>
             </tr>
         </table>
 
         <br><br><br>
+        
+        <?php if(!empty($object['object_description'])): ?>
+        <b>Description</b>
+        <br>
+        <p><?= $object['object_description'] ?></p>
+        <br><br>
+        <?php endif ?>
 
         <?php if(!empty($object['object_media'])): ?>
             <p><b>link:</b> <?= $object['object_media'] ?></p>
---
+++ manageCategories.php
@@ -0,0 +1,95 @@
+<?php
+
+/*******w******** 
+    
+    Name: Avery Kuboth
+    Description: WEBD-2013 Project - Celestial Handbook
+    Date: 2023 November 17th
+    Updated: 2023 November 17th
+
+****************/
+
+session_start();
+require('connect.php');
+define('CAT_TO_OBJ_TABLE', 'category_to_object');  
+
+// Redirect the user to index
+function redirect()
+{ 
+    header("Location: index.php");
+    die();
+}
+
+// Redirect if not logged in or unauthorized
+if($_SESSION['login_status'] !== 'loggedin' || !$_SESSION['login_account']['user_is_admin'])
+    redirect();
+
+if(!$_POST)
+    redirect();
+
+
+
+// Remove Category from an Object
+if(!empty($_POST['remove_category']))
+{
+    if (empty($_POST['category_id'])
+    || !filter_input(INPUT_POST,'category_id',FILTER_VALIDATE_INT)
+    || empty($_POST['object_id'])
+    || !filter_input(INPUT_POST,'object_id',FILTER_VALIDATE_INT))
+        redirect();
+    else
+    {
+        // Update database
+        $query = "DELETE FROM ".CAT_TO_OBJ_TABLE." WHERE category_id=:category_id AND object_id=:object_id LIMIT 1";
+        $statement = $db->prepare($query);
+        $statement->bindValue(':category_id', $_POST['category_id']);
+        $statement->bindValue(':object_id', $_POST['object_id']);
+        $statement->execute();
+
+        // Return to the Object's page
+        header("Location: fullObjectPage.php?id=". $_POST['object_id'] ."#celestial_object");
+        die();
+    }
+}
+// Add Category to an Object
+elseif(!empty($_POST['add_category']))
+{
+    if (empty($_POST['category_id'])
+    || !filter_input(INPUT_POST,'category_id',FILTER_VALIDATE_INT)
+    || empty($_POST['object_id'])
+    || !filter_input(INPUT_POST,'object_id',FILTER_VALIDATE_INT))
+        redirect();
+    else
+    {
+        echo "<br>".$_POST['category_id']."<br>";
+        echo "<br>".$_POST['object_id']."<br>";
+
+        // Search for an existing record
+        $query = "SELECT * FROM ".CAT_TO_OBJ_TABLE." WHERE category_id=:category_id AND object_id=:object_id";
+        $statement = $db->prepare($query);
+        $statement->bindValue(':category_id', $_POST['category_id']);
+        $statement->bindValue(':object_id', $_POST['object_id']);
+        $statement->execute();
+
+        if ($statement->rowCount() > 0)
+        {
+            // Return to the Object's page
+            header("Location: fullObjectPage.php?id=". $_POST['object_id'] ."#celestial_object");
+            die();
+        }
+
+        // Add new record associating the object and category
+        $query = "INSERT INTO ".CAT_TO_OBJ_TABLE." (category_id, object_id) VALUES (:category_id, :object_id)";
+        $statement = $db->prepare($query);
+        $statement->bindValue(':category_id', $_POST['category_id']);
+        $statement->bindValue(':object_id', $_POST['object_id']);
+        $statement->execute();
+
+        // Return to the Object's page
+        header("Location: fullObjectPage.php?id=". $_POST['object_id'] ."#celestial_object");
+        die();
+    }
+}
+
+
+?>
